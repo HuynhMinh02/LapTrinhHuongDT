4443e1e72186069a0abe6c530b42e1128d87800f
HuynhMinh02 <huynhuuminh908@gmail.com>
2022-01-13 16:59:01 +0700

    bài tập quản lí sinh viên

--- LapTrinhHDT/src/com/minhhh2004110025/buoi2/SinhVienGDU.java
+++ LapTrinhHDT/src/com/minhhh2004110025/buoi2/SinhVienGDU.java
@@ -3,24 +3,69 @@ package com.minhhh2004110025.buoi2;
 import java.util.Scanner;
 
 public class SinhVienGDU {
-    int id;
+    int tuoi;
     String name;
     Double diemTB;
+    String khoa;
+    String gioiTinh;
 
     Scanner scanner = new Scanner(System.in);
 
-    
-    void nhapThongTin() {   
+    public SinhVienGDU(){
+
+    }
+    public SinhVienGDU(String name, int age, String gt, String kH, Double diemTB){
+        this.name = name;
+        this.tuoi = age;
+        this.gioiTinh = gt;
+        this.khoa = kH;
+        this.diemTB = diemTB;
+    }
+    public String getName(){
+         return name;
+    }
+    public int getAge(){
+         return tuoi;
+    }
+    public String getGt(){
+        return gioiTinh;
+    }
+    public String getKhoa(){
+        return khoa;
+    }
+    public Double getDiemTB(){
+        return diemTB;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(String age){
+        this.tuoi = tuoi;
+    }
+    public void setGt(String gioiTinh){
+        this.gioiTinh = gioiTinh;
+    }
+    public void setDiemTB(double diemTB){
+        this.diemTB = diemTB;
+    }
+
+    public void nhapThongTin() {   
         System.out.println ("Nhập tên sinh viên: " );
         name = scanner.nextLine();
-        System.out.println("Nhập id: ");
-        id = scanner.nextInt();
+        System.out.println("Nhập tuoi: ");
+        tuoi = scanner.nextInt();
+        System.out.println("Nhập gioi tinh: ");
+        gioiTinh = scanner.nextLine();
+        System.out.println("Nhập khoa: ");
+        khoa = scanner.nextLine();
         System.out.println ("Nhập điểm trung bình: " );
         diemTB = scanner.nextDouble();
     }
-    void inThongTin() {
-        System.out.println("Mã sinh viên: " + id);
+    public void inThongTin() {
         System.out.println("Tên sinh vien: " + name);
+        System.out.println("Age: " + tuoi);
+        System.out.println("Giới tính: " + gioiTinh);
+        System.out.println("Nhập khoa: " + khoa);
         System.out.println ("Điểm trung bình: " + diemTB);
     }
 }
--- LapTrinhHDT/src/com/minhhh2004110025/buoi2/SinhVienGDUTestDrive.java
+++ LapTrinhHDT/src/com/minhhh2004110025/buoi2/SinhVienGDUTestDrive.java
@@ -2,9 +2,12 @@ package com.minhhh2004110025.buoi2;
 
 public class SinhVienGDUTestDrive {
     public static void main(String[] args) {
-        SinhVienGDU sinhVienGdu = new SinhVienGDU();
-
-        sinhVienGdu.nhapThongTin();
-        sinhVienGdu.inThongTin();
+        SinhVienGDU sv1 = new SinhVienGDU("Nguyễn Minh Tuán", 18, "Nam" , "Khoa CNTT", 8.0 );
+        SinhVienGDU sv2 = new SinhVienGDU("Lê Việt Đức", 18, "Nam" , "Khoa CNTT", 9.0 );
+        SinhVienGDU sv3 = new SinhVienGDU("Huỳnh Hữu Minh", 18, "Nam" , "Khoa CNTT", 8.5 );
+        
+        sv1.inThongTin();
+        sv2.inThongTin();
+        sv3.inThongTin();
     }
 }
