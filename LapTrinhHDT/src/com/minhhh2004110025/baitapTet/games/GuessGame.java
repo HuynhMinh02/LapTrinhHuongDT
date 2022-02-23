42bf605cbd62e726af81132f491d81f523e6ec5c
HuynhMinh02 <huynhuuminh908@gmail.com>
2022-02-23 21:06:44 +0700

    cập nhật encapsulation cho GussGame

--- LapTrinhHDT/src/com/minhhh2004110025/baitapTet/games/GuessGame.java
+++ LapTrinhHDT/src/com/minhhh2004110025/baitapTet/games/GuessGame.java
@@ -23,11 +23,11 @@ public class GuessGame {
             p1.guess();
             p2.guess();
             p3.guess();
-            guessp1 = p1.number;
+            guessp1 = p1.getNumber();
             System.out.println("Player one guessed " + guessp1);
-            guessp2 = p2.number;
+            guessp2 = p2.getNumber();
             System.out.println("Player two guessed " + guessp2);
-            guessp3 = p3.number;
+            guessp3 = p3.getNumber();
             System.out.println("Player three guessed " + guessp3);
             if (guessp1 == targetNumber) {
                 p1isRight = true;
--- LapTrinhHDT/src/com/minhhh2004110025/baitapTet/games/Player.java
+++ LapTrinhHDT/src/com/minhhh2004110025/baitapTet/games/Player.java
@@ -1,7 +1,11 @@
 package com.minhhh2004110025.baitapTet.games;
 
 public class Player {
-    int number = 0;
+    private int number = 0;
+
+    public int getNumber() {
+        return this.number;
+    }
     public void guess(){
         number = (int) (Math.random() *10); // đoán số trong khoảng từu 1 đến 10
         System.out.println("I'm guessing" + number);
