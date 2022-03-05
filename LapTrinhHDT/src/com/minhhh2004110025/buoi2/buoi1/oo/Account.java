da9b07d92673a2d72fb260c40f2b6d9c58b16cdb
HuynhMinh02 <huynhuuminh908@gmail.com>
2022-03-05 22:29:41 +0700

    update homework

---
+++ LapTrinhHDT/src/com/minhhh2004110025/buoi2/buoi1/oo/Account.java
@@ -0,0 +1,28 @@
+package com.minhhh2004110025.buoi2.buoi1.oo;
+
+import java.util.Scanner;
+
+public class Account {
+    int account_banlance = 0;
+    int account_number = 12345;
+
+    Scanner scanner = new Scanner(System.in);
+
+    void deposit() {
+        System.out.print("Nhập vào số tiền cần gửi: ");
+        int depositAmount = 0;
+        depositAmount = scanner.nextInt();
+        account_banlance += depositAmount; // thao tác lên data
+    }
+    void withdraw(){
+        System.out.print("Nhập vào số tiền cần rút: ");
+        int withdrawAmount = 0;
+        withdrawAmount = scanner.nextInt();
+        account_banlance -= withdrawAmount; // thao tác lên data
+    }
+    void show() {
+        System.out.println("Thông tin tài khoản");
+        System.out.println("Account number: " + account_number);
+        System.out.println ("Account balance: " + account_banlance );
+    }
+}
---
+++ LapTrinhHDT/src/com/minhhh2004110025/buoi2/buoi1/oo/EmployeeTestDrive.java
@@ -0,0 +1,44 @@
+package com.minhhh2004110025.buoi2.buoi1.oo;
+
+import java.util.Scanner;
+
+public class EmployeeTestDrive {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("|>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<|");
+            System.out.println("|>>1. Nhập tên nhân viên.                      |");
+            System.out.println("|>>2. Xuất danh sách nhân viên.                |");
+            System.out.println("|>>3. Tính lương nhân viên.                    |");
+            System.out.println("|>>4. Kết thúc! Chào tạm biệt.                 |");
+            System.out.println("|>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<|");
+            System.out.print("Hãy lựa chọn số từ 1 -> 4: ");
+            int key = scanner.nextInt();
+            scanner.nextLine();
+            switch(key){
+                case 1:
+                    System.out.println("_______Bạn đã chọn nhập thông tin nhân viên_______");
+                    employee.nhapThongTin();
+                    break;
+                case 2:
+
+                    System.out.println("_______Bạn đã chọn xuất thông tin nhân viên_______");
+                    employee.inThongTin();
+                    break;
+                case 3:
+                    System.out.println("_______Bạn đã chọn tình lương_______");
+                    employee.tinhLuong();
+                    employee.inThongTin();
+                    break;
+                case 4:
+                    System.out.println("Bạn chọn chức năng thoát! Tạm biệt!");
+                    System.exit(0);
+                default:
+                    System.out.println("Lựa chọn sai !");    
+            }
+        }while(true);   
+    }
+}
+
+
